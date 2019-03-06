6d27c8da387156fa4ace3e2a2ca75708f434a061
Danila Fedorin <danila.fedorin@gmail.com>
2019-03-05 23:30:54 -0800

    Fix segfaults in tests.

--- tests/trie_test.cpp
+++ tests/trie_test.cpp
@@ -1,6 +1,6 @@
 #include "trie_test.hpp"
 
-greatest_test_res test_trie_init() {
+greatest_test_res trie_init() {
   libab_trie trie;
   libab_trie_init(&trie);
   ASSERT_EQ(trie.head, NULL);
@@ -9,40 +9,22 @@ greatest_test_res test_trie_init() {
   PASS();
 }
 
-greatest_test_res test_trie_clear() {
+greatest_test_res trie_clear() {
   libab_trie trie;
-  libab_trie_init(&trie);
-
-  trie.empty_list.head= (ll_node_s*)malloc(sizeof(libab_trie_node));
-  trie.empty_list.tail= (ll_node_s*)malloc(sizeof(libab_trie_node));
-  trie.head = (libab_trie_node_s*)malloc(sizeof(libab_trie_node_s));
-
-  libab_trie_clear(&trie);
-
-  ASSERT_EQ(trie.head, NULL);
-  ASSERT_EQ(trie.empty_list.head, NULL);
-  ASSERT_EQ(trie.empty_list.tail, NULL);
 
   libab_trie_init(&trie);
-
-  trie.head = 0;
-  trie.empty_list.head = NULL;
-  trie.empty_list.tail = NULL;
 
   ASSERT_EQ(libab_trie_put(&trie, "derp", &trie), LIBAB_SUCCESS);
-
   GREATEST_ASSERT(trie.head != NULL);  
   ASSERT_EQ(trie.empty_list.head, NULL);
   ASSERT_EQ(trie.empty_list.tail, NULL);
 
   ASSERT_EQ(libab_trie_put(&trie, "doge", &trie), LIBAB_SUCCESS);
-
   GREATEST_ASSERT(trie.head != NULL);  
   ASSERT_EQ(trie.empty_list.head, NULL);
   ASSERT_EQ(trie.empty_list.tail, NULL);
 
   libab_trie_clear(&trie);
-
   ASSERT_EQ(trie.head, NULL);
   ASSERT_EQ(trie.empty_list.head, NULL);
   ASSERT_EQ(trie.empty_list.tail, NULL);
@@ -50,39 +32,37 @@ greatest_test_res test_trie_clear() {
   PASS();
 }
 
-greatest_test_res test_trie_put_get() {
-
+greatest_test_res trie_put_get() {
   libab_trie trie;
-
   libab_trie_init(&trie);
 
-  trie.head = 0;
-  trie.empty_list.head = NULL;
-  trie.empty_list.tail = NULL;
-
   ASSERT_EQ(libab_trie_put(&trie, "derp", &trie), LIBAB_SUCCESS);
-
   GREATEST_ASSERT(trie.head != NULL);  
   ASSERT_EQ(trie.empty_list.head, NULL);
   ASSERT_EQ(trie.empty_list.tail, NULL);
 
   ASSERT_EQ(libab_trie_put(&trie, "doge", &trie), LIBAB_SUCCESS);
-
   GREATEST_ASSERT(trie.head != NULL);  
   ASSERT_EQ(trie.empty_list.head, NULL);
   ASSERT_EQ(trie.empty_list.tail, NULL);
    
-  libab_trie *ptr = (libab_trie*)libab_trie_get(&trie, "doge"); 
-  ASSERT_EQ(ptr->empty_list.head->data, &trie);
+  ll *ptr = (ll*) libab_trie_get(&trie, "doge"); 
+  ASSERT_EQ(ptr->head->data, &trie);
 
-  ptr  = (libab_trie*)libab_trie_get(&trie, "derp");
+  ptr = (ll*) libab_trie_get(&trie, "derp");
+  ASSERT_EQ(ptr->head->data, &trie);
+
+  ptr = (ll*) libab_trie_get(&trie, "notinside");
+  ASSERT_EQ(ptr->head, NULL);
+  ASSERT_EQ(ptr->tail, NULL);
+
   libab_trie_clear(&trie);
 
   PASS();
 }
 
 SUITE(trie_suite) {
-  RUN_TEST(test_trie_init);
-  RUN_TEST(test_trie_clear);
-  RUN_TEST(test_trie_put_get);
+  RUN_TEST(trie_init);
+  RUN_TEST(trie_clear);
+  RUN_TEST(trie_put_get);
 }
